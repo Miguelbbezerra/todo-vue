ab486ff19f272547ce62166bdafc083dd34d4cc1
Carlos <carlosmiguelcm820@gmail.com>
2024-04-22 11:19:19 -0400

    function

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Content -->
+  <div class="px-3 py-10 md:px-10">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <!-- Todo spinner -->
+      <SpinnerTodo v-if="loading" />
+      <!--/ Todo spinner -->
+
+      <template v-else>
+        <!-- Todo form -->
+        <TodoFormAdd />
+        <!--/ Todo form -->
+
+        <!-- Todo items -->
+        <TodoItems v-if="$store.state.todos.length"/>
+        <!--/ Todo items -->
+
+        <!-- Todo empty -->
+        <TodoEmpty v-else/>
+        <!--/ Todo empty -->
+      </template>
+    </div>
+  </div>
+  <!--/ Content -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SpinnerTodo from "./components/SpinnerTodo.vue";
+import TodoFormAdd from "./components/TodoFormAdd.vue";
+import TodoItems from "./components/TodoItems.vue";
+import TodoEmpty from "./components/TodoEmpty.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    SpinnerTodo,
+    TodoFormAdd,
+    TodoItems,
+    TodoEmpty,
+  },
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  created() {
+    this.loading = true
+    this.$store.dispatch('getTodos').finally(() => {
+      this.loading = false
+    })
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
